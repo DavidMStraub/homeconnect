cf8c6324f807959d5ac6da284f83734909c56f26
David Straub <straub@protonmail.com>
2020-02-28 16:38:14 +0100

    Version -> 0.4.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
     LONG_DESCRIPTION = f.read()
 
 setup(name='homeconnect',
-      version='0.4.1',
+      version='0.4.2',
       author='David M. Straub',
       author_email='david.straub@tum.de',
       url='https://github.com/DavidMStraub/homeconnect',
