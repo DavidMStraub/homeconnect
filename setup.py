fdf5b7210ae764e4ccd7ddbc08fccabf1aecb148
David Straub <straub@protonmail.com>
2020-06-26 21:58:04 +0200

    [setup] Version -> 0.6, format with black

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 from setuptools import setup, find_packages
 
 
-with open('README.md') as f:
+with open("README.md") as f:
     LONG_DESCRIPTION = f.read()
 
-setup(name='homeconnect',
-      version='0.5',
-      author='David M. Straub',
-      author_email='david.straub@tum.de',
-      url='https://github.com/DavidMStraub/homeconnect',
-      description='Python client for the BSH Home Connect REST API',
-      long_description=LONG_DESCRIPTION,
-      long_description_content_type='text/markdown',
-      license='MIT',
-      packages=find_packages(),
-      install_requires=['requests', 'requests_oauthlib'],
-      extras_require={
-            'testing': ['nose',],
-      },
-    )
+setup(
+    name="homeconnect",
+    version="0.6",
+    author="David M. Straub",
+    author_email="david.straub@tum.de",
+    url="https://github.com/DavidMStraub/homeconnect",
+    description="Python client for the BSH Home Connect REST API",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=["requests", "requests_oauthlib"],
+    extras_require={"testing": ["nose",],},
+)
