6fe911a79364897f0c294ef7b133133237ee39fd
MarvinWilliam <la805779602@hotmail.com>
2021-07-21 03:41:58 +0800

    feat: support custom api url (#10)

--- homeconnect/api.py
+++ homeconnect/api.py
@@ -31,9 +31,10 @@ class HomeConnectAPI:
         client_id: str = None,
         client_secret: str = None,
         redirect_uri: str = None,
+        api_url: str = None,
         token_updater: Optional[Callable[[str], None]] = None,
     ):
-        self.host = URL_API
+        self.host = api_url if api_url else URL_API
         self.client_id = client_id
         self.client_secret = client_secret
         self.redirect_uri = redirect_uri
