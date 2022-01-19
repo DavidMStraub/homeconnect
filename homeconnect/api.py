1d6eeb9bb697241f5a625e9acd1ced7d587f6e68
Brave_Chicken <bravechickendev@gmail.com>
2022-01-19 12:13:29 +0000

    Refactor event stream to use only one

--- homeconnect/api.py
+++ homeconnect/api.py
@@ -40,6 +40,9 @@ class HomeConnectAPI:
         self.redirect_uri = redirect_uri
         self.token_updater = token_updater
 
+        self._appliances = []
+        self.listening_events = False
+
         extra = {"client_id": self.client_id, "client_secret": self.client_secret}
 
         self._oauth = OAuth2Session(
@@ -127,8 +130,25 @@ class HomeConnectAPI:
     def get_appliances(self):
         """Return a list of `HomeConnectAppliance` instances for all
         appliances."""
+
+        appliances = []
+
         data = self.get(ENDPOINT_APPLIANCES)
-        return [HomeConnectAppliance(self, **app) for app in data["homeappliances"]]
+        for home_appliance in data["homeappliances"]:
+            found_matching = False
+            for appliance in self._appliances:
+                if appliance.haId == home_appliance["haId"]:
+                    appliance.connected = home_appliance["connected"]
+                    appliances += appliance
+                    found_matching = True
+                    break
+
+            if not found_matching:
+                appliances += [HomeConnectAppliance(self, **home_appliance)]
+                continue
+
+        self._appliances = appliances
+        return self._appliances
 
     def get_authurl(self):
         """Get the URL needed for the authorization code grant flow."""
@@ -136,6 +156,50 @@ class HomeConnectAPI:
             f"{self.host}/{ENDPOINT_AUTHORIZE}"
         )
         return authorization_url
+
+    def listen_events(self):
+        """Spawn a thread with an event listener that updates the status."""
+        self.listening_events = True
+        uri = f"{self.host}/api/homeappliances/events"
+        sse = SSEClient(uri, session=self._oauth, retry=1000, timeout=TIMEOUT_S)
+        Thread(target=self._listen, args=[sse]).start()
+
+    def _listen(self, sse):
+        """Worker function for listener."""
+        LOGGER.info("Listening to event stream for all devices")
+        try:
+            for event in sse:
+                try:
+                    for appliance in self._appliances:
+                        if appliance.haId == event.id:
+                            self.handle_event(event, appliance)
+                            break
+                except ValueError:
+                    pass
+        # except TokenExpiredError as e:
+        except TokenExpiredError:
+            LOGGER.info("Token expired in event stream.")
+            self._oauth.token = self.refresh_tokens()
+            uri = f"{self.host}/api/homeappliances/events"
+            sse = SSEClient(uri, session=self._oauth, retry=1000, timeout=TIMEOUT_S)
+            self._listen(sse)
+
+    def handle_event(self, event, appliance):
+        """Handle a new event.
+
+        Updates the status with the event data and executes any callback
+        function."""
+        event_data = json.loads(event.data)
+        d = self.json2dict(event_data["items"])
+        appliance.status.update(d)
+        if appliance.event_callback is not None:
+            appliance.event_callback(appliance)
+
+    @staticmethod
+    def json2dict(lst):
+        """Turn a list of dictionaries where one key is called 'key'
+        into a dictionary with the value of 'key' as key."""
+        return {d.pop("key"): d for d in lst}
 
 
 class HomeConnect(HomeConnectAPI):
@@ -203,6 +267,8 @@ class HomeConnectAppliance:
         self.connected = connected
         self.status = {}
 
+        self.event_callback = None
+
     def __repr__(self):
         return "HomeConnectAppliance(hc, haId='{}', vib='{}', brand='{}', type='{}', name='{}', enumber='{}', connected={})".format(
             self.haId,
@@ -215,43 +281,17 @@ class HomeConnectAppliance:
         )
 
     def listen_events(self, callback=None):
-        """Spawn a thread with an event listener that updates the status."""
-        uri = f"{self.hc.host}/api/homeappliances/{self.haId}/events"
-        sse = SSEClient(uri, session=self.hc._oauth, retry=1000, timeout=TIMEOUT_S)
-        Thread(target=self._listen, args=(sse, callback)).start()
+        """Register event callback method"""
+        self.event_callback = callback
 
-    def _listen(self, sse, callback=None):
-        """Worker function for listener."""
-        LOGGER.info("Listening to event stream for device %s", self.name)
-        try:
-            for event in sse:
-                try:
-                    self.handle_event(event, callback)
-                except ValueError:
-                    pass
-        except TokenExpiredError as e:
-            LOGGER.info("Token expired in event stream.")
-            self.hc._oauth.token = self.hc.refresh_tokens()
-            uri = f"{self.hc.host}/api/homeappliances/{self.haId}/events"
-            sse = SSEClient(uri, session=self.hc._oauth, retry=1000, timeout=TIMEOUT_S)
-            self._listen(sse, callback=callback)
+        if not self.hc.listening_events:
+            self.hc.listen_events()
 
     @staticmethod
     def json2dict(lst):
         """Turn a list of dictionaries where one key is called 'key'
         into a dictionary with the value of 'key' as key."""
         return {d.pop("key"): d for d in lst}
-
-    def handle_event(self, event, callback=None):
-        """Handle a new event.
-
-        Updates the status with the event data and executes any callback
-        function."""
-        event = json.loads(event.data)
-        d = self.json2dict(event["items"])
-        self.status.update(d)
-        if callback is not None:
-            callback(self)
 
     def get(self, endpoint):
         """Get data (as dictionary) from an endpoint."""
@@ -359,6 +399,6 @@ class HomeConnectAppliance:
     def execute_command(self, command):
         """Execute a command."""
         return self.put(
-            f"/commands/{command}", {"data": {"key": command, "value": True}},
+            f"/commands/{command}",
+            {"data": {"key": command, "value": True}},
         )
-
